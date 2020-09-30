f332bd9a70cd1e3573825481d543336527311f5f
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-09-30 21:27:16 +0200

    Tercer paso del patrón Singleton
    Creación del método estático que controle que se crea una y solo una instancia y la cree si no se ha creado.
    Devolverá la única instancia posible.

--- PatronSingleton_VAT/src/ule/iso1/singleton/VATtoApply.java
+++ PatronSingleton_VAT/src/ule/iso1/singleton/VATtoApply.java
@@ -25,6 +25,21 @@ public class VATtoApply {
 	public void setVatType(double vatType) {
 		this.vatType = vatType;
 	}
+	
+	public static VATtoApply getInstance(double vat) { 			//publico porque se tiene que poder ver desde el Main
+																//estático porque se tiene que poder instanciar sin haber creado una instancia de esta clase (porque no se puede al haber privatizado el constructor)
+		//Controlamos que solamente se crea una instancia aunque llamemos infinitas veces al método
+		if(uniqueInstance == null) {	//si la variable es vacía aún no se ha creado ninguna instancia
+			
+			uniqueInstance = new VATtoApply(vat); //llamo al constructor para crear la instancia y la inserto en el atributo
+						
+			
+		}
+		
+		//si el atributo no es nulo, es que tiene ya una instancia, sea porque la tenia de una llamada anterior a este metodo o porque la acabamos de crear, luego devolvemos esa
+		return uniqueInstance;	
+		
+	}	
 
 }
 
@@ -32,11 +47,6 @@ public class VATtoApply {
  * 
  * 1-Crear una variable privada que sea la única instancia
  * 2-El constructor tiene que ser privado
- * 
- * 
- * 
- * 
- * 
- * 
+ * 3-Crear un método que se encargue de crear esa única instancia
  * 
  */
